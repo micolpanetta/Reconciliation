c65c9ddaabefe67d9d0518a68eb045c7b13a7e1e
Micol Panetta <micol.panetta@intervieweb.it>
2023-01-29 11:22:00 +0100

    Add project files.

---
+++ Reconciliation/Program.cs
@@ -0,0 +1,58 @@
+﻿namespace Reconciliation
+{
+    internal class Program
+    {
+        static readonly string rootFolder = "C:\\Temp\\Files";    
+        static readonly string paymentsFile = "C:\\Temp\\Files\\Payments.json";
+        static readonly string pricesFile = "C:\\Temp\\Files\\Prices.xml";
+        static readonly string purchasesFile = "C:\\Temp\\Files\\Purchases.dat";
+        static int lineIndex = 0;
+
+        static void Main(string[] args)
+        {
+            if (File.Exists(purchasesFile))
+            {
+                string[] lines = File.ReadAllLines(purchasesFile);
+                
+                List<Purchase> purchases = new List<Purchase>();
+                
+                while (lineIndex < lines.Length)
+                {
+                    Purchase purchase = new Purchase
+                    {
+                        CustomerId = lines[lineIndex].Substring(4),
+                        Date = ExtractDate(lines[lineIndex + 1]),
+                        ItemIds = ExtractItemIds(lines),
+                    };
+
+                    purchases.Add(purchase);
+                }
+                purchases.ForEach(Console.WriteLine);
+            }
+        }
+
+        private static List<String> ExtractItemIds(string[] lines)
+        {
+            List<String> ItemIds = new List<String>();
+
+            lineIndex += 2;
+
+            while (lineIndex < lines.Length && lines[lineIndex].StartsWith("ITEM")) {
+                ItemIds.Add(lines[lineIndex].Substring(4));
+                lineIndex++;
+            }
+          
+            return ItemIds;
+        }
+
+        private static DateTime ExtractDate(String date)
+        {
+            int day = int.Parse(date.Substring(4, 2));
+            int month = int.Parse(date.Substring(6, 2));
+            int year = int.Parse(date.Substring(8, 4));
+
+            return new DateTime(year, month, day);  
+
+        }
+    }
+}
---
+++ Reconciliation/Purchase.cs
@@ -0,0 +1,43 @@
+﻿using System.Xml.Linq;
+
+namespace Reconciliation
+{
+    internal class Purchase
+    {
+        private String _customerId { get; set; }
+        private DateTime _date { get; set; }
+        private List<String> _itemIds { get; set; }
+        
+        public String CustomerId
+        {
+            get => _customerId;
+            set => _customerId = value;
+        }
+        public DateTime Date
+        {
+            get => _date;
+            set => _date = value;
+        }
+        public List<String> ItemIds
+        {
+            get => _itemIds;
+            set => _itemIds = value;
+        }
+
+        public override string? ToString()
+        {
+            return "CustomerId: " + CustomerId + " Date: " + Date + " ItemIds: " + string.Join(",", ItemIds);
+        }
+
+        //public override string ToString()
+        //{
+        //    return GetType().GetProperties()
+        //        .Select(info => (info.Name, Value: info.GetValue(this, null) ?? "(null)"))
+        //        .Aggregate(
+        //            new StringBuilder(),
+        //            (sb, pair) => sb.AppendLine($"{pair.Name}: {pair.Value}"),
+        //            sb => sb.ToString());
+        //}
+    }
+
+}
