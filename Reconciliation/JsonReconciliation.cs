21eeb4023e212348192e365efcf69feb4cbd2f0a
Micol Panetta <micol.panetta@intervieweb.it>
2023-01-30 23:25:49 +0100

    added todo

--- Reconciliation/JsonReconciliation.cs
+++ Reconciliation/JsonReconciliation.cs
@@ -7,7 +7,7 @@ namespace Reconciliation
         public void printReconciliation(List<Reconciliation> reconciliations)
         {
             var jsonString = JsonConvert.SerializeObject(reconciliations);
-            File.WriteAllText(Environment.GetEnvironmentVariable("FILESPATH") + "\\PaymentsNotMatched.json", jsonString);
+            File.WriteAllText(Environment.GetEnvironmentVariable("FILESPATH") + "\\PaymentsNotMatched.json", jsonString); //TODO creare cartella per output
         }
     }
 }
